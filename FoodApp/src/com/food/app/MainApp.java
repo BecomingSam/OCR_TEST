62467d59c2147dc5aa3659383aa1cd2ca2dd5b1c
BecomingSam <sam.s.li@outlook.com>
2013-10-30 23:57:03 -0400

    Libraries

--- FoodApp/src/com/food/app/MainApp.java
+++ FoodApp/src/com/food/app/MainApp.java
@@ -1,9 +1,15 @@
 package com.food.app;
+
+import com.food.ocr.OCRApiService;
+import com.food.ocr.OCRService;
+
 public class MainApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		OCRService service = new OCRApiService();
+		String result = service.convertToText("en", "C:/test.jpg");
+		
+		System.out.print(result);
 	}
 
 }
